aeb4facebc8193c1de0276239718b6449e44d0b3
fukaya <>
2021-07-22 17:23:04 +0900

    make it better

---
+++ app/Listing.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    //
+    //hasMany設定
+    public function cards()
+    {
+        return $this->hasMany('App\Card');
+    }
+}
